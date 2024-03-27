1ab4b38a5624e0c3a7915431d39c6aad161e4c7b
Pradhumn Chandrawat <chandrawatpradhumn@gmail.com>
2024-03-27 22:31:06 +0530

    product cart assignment

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const Products = [
+  { id: 1, name: "Product-1", price: 100, quatitySelected: 0 },
+  { id: 2, name: "Product-2", price: 200, quatitySelected: 0 },
+  { id: 3, name: "Product-3", price: 300, quatitySelected: 0 },
+  { id: 4, name: "Product-4", price: 400, quatitySelected: 0 },
+  { id: 5, name: "Product-5", price: 500, quatitySelected: 0 },
+];
+
+export default Products;
